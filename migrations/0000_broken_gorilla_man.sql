70e1a5f631c74448e6fec078af740fcc48c965a9
alexhaag <alexhaag@hotmail.com>
2026-02-02 22:42:21 -0300

    first upload

---
+++ migrations/0000_broken_gorilla_man.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "leads" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"phone" text,
+	"message" text,
+	"created_at" timestamp DEFAULT now()
+);
